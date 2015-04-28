dfe70e67a8f009ab02a0d4c152ae7bfa44ca9b2e
tcandens <joey.thies@gmail.com>
2015-04-28 12:47:19 -0700

    Add Gruntfile working test and jshint tasks

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    jshint: {
+      files: ['test/*.js', 'lib/*.js']
+    },
+    mochaTest: {
+      options: {
+        quite: false
+      },
+      src: ['test/**/*.js']
+    }
+  });
+  // grunt.registerTask('default', []);
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('jshint' ['jshint'])
+}
